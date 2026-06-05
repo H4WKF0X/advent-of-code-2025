3d647d17687087e23b515c9cf70b470866ca1c23
H4WKF0X <maximilianpaesold@gmail.com>
2026-06-05 23:07:37 +0200

    day03 initial setup

---
+++ src/main/java/aoc/day03/Day03.java
@@ -0,0 +1,21 @@
+package aoc.day03;
+
+import aoc.util.Input;
+
+import java.util.List;
+
+public class Day03 {
+    public static void main(String[] args) {
+        List<String> input = Input.sampleLines(3);   // swap to Input.sampleLines(3) while debugging
+        System.out.println("Part 1: " + part1(input));
+        System.out.println("Part 2: " + part2(input));
+    }
+
+    static Object part1(List<String> input) {
+        return 0;
+    }
+
+    static Object part2(List<String> input) {
+        return 0;
+    }
+}
